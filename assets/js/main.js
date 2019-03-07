12a83f145c12362cdc7b6e0d39499e3eeaf19b5e
Davi Alves <alvesdelima.davi@gmail.com>
2019-03-07 01:56:19 -0300

    still a lot to do

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
     $('.modal').modal();
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.carousel').carousel();
     $('.sidenav').sidenav();
   });
+
+  function smoothScrollBegin() {
+    var element = document.getElementById("inicio");
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
+  }
+  function smoothScrollRegion() {
+    var element = document.getElementById("regioes");
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
+  }
+  function smoothScrollContact() {
+    var element = document.getElementById("contato");
+    element.scrollIntoView({ behavior: "smooth", block: "end" });
+  }
